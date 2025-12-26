2afae769048874d09ac830f4112328f35e0fe1eb
anvolkov1 <anvolkov1@mts.ru>
2025-12-26 06:14:29 +0300

    next ver

--- src/main/java/com/example/generation/Main.java
+++ src/main/java/com/example/generation/Main.java
@@ -1,11 +1,12 @@
 package com.example.generation;
 
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int year = 1997;
-
-        System.out.print("Your year of birth: " + year);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your year of birth: ");
+        int year = scanner.nextInt();
 
         String generation = GenerationCalculation.getGeneration(year);
         System.out.println("Your generation is: " + generation);
